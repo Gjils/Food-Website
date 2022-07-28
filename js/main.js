4d0313cf5b82145729e824f1ab2adcc4bb7cca7c
Gjils <preobrazhenskyalexsey@gmail.com>
2022-07-28 11:42:29 +0300

    Copied layout, added tabs switching

---
+++ js/main.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", () => {
+  // Tabs
+
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabsContent.forEach((tab) => {
+      tab.classList.add("hide");
+      tab.classList.remove("show", "fade");
+    });
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.remove("hide");
+    tabsContent[i].classList.add("fade", "show");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.matches(".tabheader__item")) {
+      tabs.forEach((tab, i) => {
+        if (tab == target) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  hideTabContent();
+  showTabContent();
+});
