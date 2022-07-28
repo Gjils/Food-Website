5c4766d8cd3a4abaf4aec345c54227ecd4fe3afe
Gjils <preobrazhenskyalexsey@gmail.com>
2022-07-28 16:47:29 +0300

    Added time count for the timer in the bottom of page

--- js/main.js
+++ js/main.js
@@ -36,4 +36,54 @@ window.addEventListener("DOMContentLoaded", () => {
 
   hideTabContent();
   showTabContent();
+
+  // Timer
+
+  function getZero(num) {
+    return num >= 0 && num < 10 ? "0" + num : num;
+  }
+  const endtime = "2023-05-20";
+
+  function getTimeRemaining(endtime) {
+    let days = 0,
+      hours = 0,
+      minutes = 0,
+      seconds = 0;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    if (t > 0) {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor((t / (1000 * 60 * 60)) % days);
+      minutes = Math.floor((t / (1000 * 60)) % 60);
+      seconds = Math.floor((t / 1000) % 60);
+    }
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+    updateClock();
+  }
+  setClock(".timer", endtime);
 });
