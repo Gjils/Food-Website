6ac70caf40e73adfba9f850f7588fcf8b17347c4
Gjils <preobrazhenskyalexsey@gmail.com>
2022-07-30 16:28:30 +0300

    Request returns PHP code

--- js/main.js
+++ js/main.js
@@ -196,4 +196,46 @@ window.addEventListener("DOMContentLoaded", () => {
   menuItems.forEach((item, i) => {
     new MenuCard(menuParsedItems[i]).render();
   });
+
+  // Forms
+
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "Loading",
+    success: "Success",
+    failure: "Something went wrong",
+  };
+
+  forms.forEach((item) => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open("POST", "server.php");
+      // request.setRequestHeader("Content-type", "multipart/form-data");
+
+      const formData = new FormData(form);
+      request.send(formData);
+
+      request.addEventListener("load", () => {
+        if (request.status == 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+        } else {
+          console.log(`Error ${request.status}`);
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
